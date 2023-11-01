9f28ad1e8c6c97e710debcd0b4773e25c5b983e8
orangeshop <rhkeh123@naver.com>
2023-11-01 14:47:10 +0900

    Initial Commit

---
+++ diary_app/diary_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  diary_appApp.swift
+//  diary_app
+//
+//  Created by 최영호 on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct diary_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
